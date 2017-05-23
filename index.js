83d6439e0cd5ee76f79024f019b2e0ca4a4352ea
Jay Wiggins <jay@irk.com>
2017-05-23 11:18:59 -0700

    trying something new

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 'use strict';
+var express = require('express');
+var app = express();
+
+var path = require('path');
 var fs = require('fs');
 
- exports.get = function(event, context) {
-   var contents = fs.readFileSync("public/index.html");
-   context.succeed({
-     statusCode: 200,
-     body: contents.toString(),
-     headers: {'Content-Type': 'text/html'}
-   });
- };
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+
+module.exports = app;
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
+ // exports.get = function(event, context) {
+ //   var contents = fs.readFileSync("public/index.html");
+ //   context.succeed({
+ //     statusCode: 200,
+ //     body: contents.toString(),
+ //     headers: {'Content-Type': 'text/html'}
+ //   });
+ // };
